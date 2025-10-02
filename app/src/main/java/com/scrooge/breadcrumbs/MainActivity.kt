d0289a1eab1f85b0c75c986e8d587f95dfeb1e07
scrooge313 <151065324+scrooge313@users.noreply.github.com>
2025-10-02 22:17:11 +0200

    consider status bar

--- app/src/main/java/com/scrooge/breadcrumbs/MainActivity.kt
+++ app/src/main/java/com/scrooge/breadcrumbs/MainActivity.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import com.scrooge.breadcrumbs.overview.ui.Overview
 import com.scrooge.breadcrumbs.core.ui.theme.BreadcrumbsTheme
 
@@ -15,8 +24,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BreadcrumbsTheme {
+                val layoutDirection = LocalLayoutDirection.current
                 Overview(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding() // for not overlapping the status bar
+                        .padding( // for not overlapping the status bar
+                            start = WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateStartPadding(layoutDirection),
+                            end = WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateEndPadding(layoutDirection)
+                        )
                 )
             }
         }
